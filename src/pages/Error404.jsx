bab95174ee9bb843df4b5b33b8ee6bf5b9dd195c
Willy <Willytoc@gmail.com>
2023-01-19 14:55:19 -0500

    Prueba

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -5,7 +5,7 @@ const Error404 = () => {
   return (
     <Wrapper className='container'>
       <h1>En Construccion</h1>
-      <h2>Estoy trabajando fuertemente para completar mi portafolio.</h2>
+      <h2>Estoy trabajando fuertemente para completar mi portafolio. X2</h2>
       <img src='construccion.jpg' alt='Construyendo mi Portafolio' />
     </Wrapper>
   )
