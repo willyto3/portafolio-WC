9f320b400ad16558ac74c0d8b59dfaff7abe6c22
Willy <Willytoc@gmail.com>
2023-01-20 16:34:51 -0500

    Cambio de Foto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <Wrapper className='container'>
       <div className='box grid grid-2-columnas'>
         <div className='box__columna'>
-          <img src='willyBlanco.jpg' alt='Foto Perfil' />
+          <img src='WillyNegro.jpg' alt='Foto Perfil' />
         </div>
         <div className='box__columna'>
           <Presentacion />
